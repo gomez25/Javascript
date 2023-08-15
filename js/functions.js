2475446298955cff1c1c7b2954405cbd392fd1ad
David Gomez <dgz2903@gmail.com>
2023-08-15 11:53:23 -0600

    Tercera entrega

---
+++ js/functions.js
@@ -0,0 +1,168 @@
+//Constantes
+const porcentajeCcss = 0.1067;
+
+//Variables 
+let salarioInput = document.getElementById("salario");
+let tipoCambioInput = document.getElementById("tipoCambio");
+let dropdownItems = document.querySelectorAll('.dropdown-item');
+let btnTipoCambio = document.getElementById('btnTipoCambio');
+let tipoCambio = "Dolares";
+
+//Formato de los numeros
+let formatoNumeros = (numero) => {
+    const numeroFormateado = new Intl.NumberFormat('es-CR', {
+        style: 'currency',
+        currency: 'CRC',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(numero);
+
+    return numeroFormateado;
+}
+
+// Calcula la deduccion de la CCSS
+let calculoDeduccionCaja = (salario) => {
+    return (salario * porcentajeCcss);
+}
+
+// Calcula salario sin deducciones
+let calculoSalarioBruto = (salario) => {
+    return salario;
+}
+
+//Calculo del salario neto
+let calculoSalarioNeto = (salario) => {
+    tractos.forEach(tracto => {
+        salario -= tracto.montoDeduccion;
+    });
+    return salario;
+}
+
+//Calcula la renta de cada uno de los trctos
+let calculoRenta = (salario, tracto) => {
+    if (salario >= tracto.minimo) {
+        return ((salario - tracto.minimo) * tracto.deduccion)
+    }
+    return 0;
+}
+
+let calculoRentaSinMaximo = (salario, tracto) => {
+    if (salario >= tracto.minimo) {
+        return ((salario - tracto.minimo) * tracto.deduccion)
+    }
+    return 0;
+}
+
+//Calcula la renta de cada uno de los tractos
+let calculoRentaTotal = () => {
+    let respuesta = 0;
+    tractos.forEach(tracto => {
+        respuesta += tracto.montoDeduccion;
+    });
+    return respuesta;
+}
+
+//Calcula total de deducciones
+let calculoDeduccionesTotales = (salario) => {
+    return (calculoDeduccionCaja(salario) + calculoRentaTotal());
+}
+
+let calculoSalarioTipoCambio = (salario) => {
+    if (tipoCambio == "Dolares") {
+        return salario * tipoCambioInput.value;
+    } else {
+        return salario;
+    }
+}
+
+//Lista de los tractos de deduccion de renta
+let tractos = [
+    { id: "exonerado", maximo: 941000, montoDeduccion: 0 },
+    { id: "renta10", minimo: 941000, maximo: 1381000, deduccion: 0.10, montoDeduccion: 0 },
+    { id: "renta15", minimo: 1381000, maximo: 2423000, deduccion: 0.15, montoDeduccion: 0 },
+    { id: "renta20", minimo: 2423000, maximo: 4845000, deduccion: 0.20, montoDeduccion: 0 },
+    { id: "renta25", minimo: 4845000, deduccion: 0.25, montoDeduccion: 0 }
+]
+
+//Lista del detalle
+let detalles = [
+    { id: "salarioBruto", monto: 0, metodoCalcular: calculoSalarioBruto },
+    { id: "impuestosRenta", monto: 0, metodoCalcular: calculoRentaTotal },
+    { id: "caja", monto: 0, metodoCalcular: calculoDeduccionCaja },
+    { id: "totalDeducciones", monto: 0, metodoCalcular: calculoDeduccionesTotales },
+    { id: "salarioNeto", monto: 0, metodoCalcular: calculoSalarioNeto }
+]
+
+//Llenar tablas
+//Crea la tabla de los tractos despues de calcular los montos de deduccion
+let insertarLinea = (id, monto) => {
+    const cell = document.getElementById(id);
+    cell.innerHTML = formatoNumeros(monto);
+}
+
+//Con este metodo se calcula lo que paga de renta por cada tracto
+// y se guarda en el monto de deduccion de cada objeto
+let calculoDetractos = (salario) => {
+    tractos.forEach(tracto => {
+        if (!tracto.hasOwnProperty('minimo')) {
+            tracto.montoDeduccion = 0;
+        } else {
+            tracto.montoDeduccion = calculoRenta(salario, tracto);
+        }
+        insertarLinea(tracto.id, tracto.montoDeduccion);
+    });
+}
+
+// Se rellena el detalle para posteriormente crear y llenar la tabla
+let llenarDetalle = (salario) => {
+    detalles.forEach(detalle => {
+        if (typeof detalle.metodoCalcular === "function") {
+            if (detalle.metodoCalcular.length === 0) {
+                detalle.monto = detalle.metodoCalcular();
+            }
+            else {
+                detalle.monto = detalle.metodoCalcular(salario);
+            }
+        }
+        insertarLinea(detalle.id, detalle.monto);
+    });
+}
+
+// Agregar funcion para el boton del dropdown
+dropdownItems.forEach(item => {
+    item.addEventListener('click', function () {
+        const selectedValue = this.textContent;
+        btnTipoCambio.innerHTML = selectedValue;
+        tipoCambio = selectedValue;
+        if (selectedValue == "Dolares") {
+            tipoCambioInput.style.display = 'inline';
+        } else {
+            tipoCambioInput.style.display = 'none';
+        }
+    });
+});
+
+//Metodo principal
+function Calcular() {
+    let salario = salarioInput.value;
+    calculoDetractos(calculoSalarioTipoCambio(salario));
+    console.error("Hola");
+    llenarDetalle(calculoSalarioTipoCambio(salario));
+}
+
+//Metodo tipo cambio
+function fetchTipoCambio() {
+    fetch('https://test.sistemamanejoseguros.com/api/tipocambio')
+      .then(response => response.json())
+      .then(data => {
+        tipoCambioInput.value = data;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchTipoCambio();
+  });
+  
--- js/index.js
+++ js/index.js
@@ -1,150 +1,239 @@
 //Constantes
-const porcentajeCcss = 0.1067;
+const doc = document;
+const head = doc.head;
+const body = doc.body;
+const title = doc.title;
+title.innerHTML = "Proyecto Final David Gomez"
 
-//Variables 
-let salarioInput = document.getElementById("salario");
-let tipoCambioInput = document.getElementById("tipoCambio");
-let dropdownItems = document.querySelectorAll('.dropdown-item');
-let btnTipoCambio = document.getElementById('btnTipoCambio');
-let tipoCambio = "Dolares";
-
-//Formato de los numeros
-let formatoNumeros = (numero) => {
-    const numeroFormateado =  new Intl.NumberFormat('es-CR', {
-        style: 'currency',
-        currency: 'CRC',
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      }).format(numero);
-
-      return numeroFormateado;
-}
-
-// Calcula la deduccion de la CCSS
-let calculoDeduccionCaja = (salario) => {
-    return (salario * porcentajeCcss);
-}
-
-// Calcula salario sin deducciones
-let calculoSalarioBruto = (salario) => {
-    return salario;
-}
-
-//Calculo del salario neto
-let calculoSalarioNeto = (salario) => {
-    tractos.forEach(tracto => {
-        salario -= tracto.montoDeduccion;
-    });
-    return salario;
-}
-
-//Calcula la renta de cada uno de los trctos
-let calculoRenta = (salario, tracto) => {
-    if (salario >= tracto.minimo) {
-        return ((salario - tracto.minimo) * tracto.deduccion)
+///////////////////Inicio Navbar//////////////////////////
+//Crear elementos del NavBar
+const header = doc.createElement('header');
+const footer = doc.createElement('footer');
+const divNav = doc.createElement('div');
+const navBar = doc.createElement('nav');
+const ul = doc.createElement('ul');
+const links = [
+    {
+        page: 'index',
+        link: 'Inicio'
+    },
+    {
+        page: 'pages/proyectos',
+        link: 'Proyectos'
     }
-    return 0;
-}
+];
+body.prepend(header)
+header.append(navBar)
+navBar.appendChild(divNav)
+divNav.appendChild(ul)
 
-let calculoRentaSinMaximo = (salario, tracto) => {
-    if (salario >= tracto.minimo) {
-        return ((salario - tracto.minimo) * tracto.deduccion)
-    }
-    return 0;
-}
-
-//Calcula la renta de cada uno de los tractos
-let calculoRentaTotal = () => {
-    let respuesta = 0;
-    tractos.forEach(tracto => {
-        respuesta += tracto.montoDeduccion;
-    });
-    return respuesta;
-}
-
-//Calcula total de deducciones
-let calculoDeduccionesTotales = (salario) => {
-    return (calculoDeduccionCaja(salario) + calculoRentaTotal());
-}
-
-let calculoSalarioTipoCambio = (salario) => {
-    if (tipoCambio == "Dolares") {
-        return salario * tipoCambioInput.value;
-    } else {
-        return salario;
-    }
-}
-
-//Lista de los tractos de deduccion de renta
-let tractos = [
-    { id: "exonerado", maximo: 941000, montoDeduccion: 0 },
-    { id: "renta10", minimo: 941000, maximo: 1381000, deduccion: 0.10, montoDeduccion: 0 },
-    { id: "renta15", minimo: 1381000, maximo: 2423000, deduccion: 0.15, montoDeduccion: 0 },
-    { id: "renta20", minimo: 2423000, maximo: 4845000, deduccion: 0.20, montoDeduccion: 0 },
-    { id: "renta25", minimo: 4845000, deduccion: 0.25, montoDeduccion: 0 }
-]
-
-//Lista del detalle
-let detalles = [
-    { id: "salarioBruto", monto: 0, metodoCalcular: calculoSalarioBruto },
-    { id: "impuestosRenta", monto: 0, metodoCalcular: calculoRentaTotal },
-    { id: "caja", monto: 0, metodoCalcular: calculoDeduccionCaja },
-    { id: "totalDeducciones", monto: 0, metodoCalcular: calculoDeduccionesTotales },
-    { id: "salarioNeto", monto: 0, metodoCalcular: calculoSalarioNeto }
-]
-
-//Llenar tablas
-//Crea la tabla de los tractos despues de calcular los montos de deduccion
-let insertarLinea = (id, monto) => {
-    const cell = document.getElementById(id);
-    cell.innerHTML = formatoNumeros(monto);
-}
-
-//Con este metodo se calcula lo que paga de renta por cada tracto
-// y se guarda en el monto de deduccion de cada objeto
-let calculoDetractos = (salario) => {
-    tractos.forEach(tracto => {
-        if (!tracto.hasOwnProperty('minimo')) {
-            tracto.montoDeduccion = 0;
-        } else {
-            tracto.montoDeduccion = calculoRenta(salario, tracto);
-        }
-        insertarLinea(tracto.id, tracto.montoDeduccion);
-    });
-}
-
-// Se rellena el detalle para posteriormente crear y llenar la tabla
-let llenarDetalle = (salario) => {
-    detalles.forEach(detalle => {
-        if (typeof detalle.metodoCalcular === "function") {
-            if (detalle.metodoCalcular.length === 0) {
-                detalle.monto = detalle.metodoCalcular();
-            }
-            else {
-                detalle.monto = detalle.metodoCalcular(salario);
-            }
-        }
-        insertarLinea(detalle.id, detalle.monto);
-    });
-}
-
-// Agregar funcion para el boton del dropdown
-dropdownItems.forEach(item => {
-  item.addEventListener('click', function() {
-    const selectedValue = this.textContent;
-    btnTipoCambio.innerHTML = selectedValue;
-    tipoCambio = selectedValue;
-    if(selectedValue == "Dolares"){
-        tipoCambioInput.style.display = 'inline';
-    }else{
-        tipoCambioInput.style.display = 'none';
-    }
-  });
+//Rellenar NavBar   
+links.forEach((nombre) => {
+    ul.innerHTML += `
+                    <li class="nav-item">
+                        <a class="nav-link" href="/${nombre.page}.html">${nombre.link}</a> 
+                    </li>
+    `
 });
 
-//Metodo principal
-function Calcular() {
-    let salario = salarioInput.value;
-    calculoDetractos(calculoSalarioTipoCambio(salario));
-    llenarDetalle(calculoSalarioTipoCambio(salario));
-}
+//Crear estilos 
+navBar.classList = 'navbar navbar-expand-lg navbar-dark bg-dark'
+divNav.classList = 'container-fluid'
+ul.classList = 'navbar-nav'
+
+///////////////////Fin Navbar//////////////////////////
+
+///////////////////Inicio Body//////////////////////////
+
+//Crear elementos del body
+const main = doc.createElement('main');
+const bodyTitle = doc.createElement('h1');
+const bodySubtitle = doc.createElement('h3');
+const bodyInputsSection = doc.createElement('section');
+const bodyDivContainer = doc.createElement('div');
+const bodyDivSubContainer = doc.createElement('div');
+const bodySalarioInput = doc.createElement('input');
+const bodySalarioButton = doc.createElement('button');
+const bodySalarioUl = doc.createElement('ul');
+const bodyTipoCambioInput = doc.createElement('input');
+const bodyTyDSection = doc.createElement('section');
+const bodyTractosTitle = doc.createElement('h2');
+const bodyTractosTable = doc.createElement('table');
+const bodyTractosTHead = doc.createElement('thead');
+const bodyTractosTbody = doc.createElement('tbody');
+const bodyDetallesTitle = doc.createElement('h2');
+const bodyDetallesTable = doc.createElement('table');
+const bodyDetallesTHead = doc.createElement('thead');
+const bodyDetallesTbody = doc.createElement('tbody');
+
+const listaSalarioDropDown = [
+    {
+        value: 'Colones'
+    },
+    {
+        value: 'Dolares'
+    }
+];
+
+const listaTractosTd = [
+    {
+        nombre: 'Hasta ₡941,000.00',
+        porcentaje: '0%',
+        monto: '₡0.00',
+        id: 'exonerado'
+    },
+    {
+        nombre: 'Sobre el excedente de ₡941,000.00 hasta ₡1,381,000.00',
+        porcentaje: '10%',
+        monto: '₡0.00',
+        id: 'renta10'
+    },
+    {
+        nombre: 'Sobre el excedente de ₡1,381,000.00 hasta ₡2,423,000.00',
+        porcentaje: '15%',
+        monto: '₡0.00',
+        id: 'renta15'
+    },
+    {
+        nombre: 'Sobre el excedente de ₡2,423,000.00 hasta ₡4,845,000.00',
+        porcentaje: '20%',
+        monto: '₡0.00',
+        id: 'renta20'
+    },
+    {
+        nombre: 'Sobre el excedente de ₡4,845,000.00',
+        porcentaje: '25%',
+        monto: '₡0.00',
+        id: 'renta25'
+    }
+];
+
+const listaDetallesTd = [
+    {
+        nombre: 'Salario Bruto:',
+        monto: '₡0.00',
+        id: 'salarioBruto'
+    },
+    {
+        nombre: 'Impuestos sobre la Renta:',
+        monto: '₡0.00',
+        id: 'impuestosRenta'
+    },
+    {
+        nombre: 'CCSS: 10.67%',
+        monto: '₡0.00',
+        id: 'caja'
+    },
+    {
+        nombre: 'Total de Deducciones:',
+        monto: '₡0.00',
+        id: 'totalDeducciones'
+    },
+    {
+        nombre: 'Salario Neto:',
+        monto: '₡0.00',
+        id: 'salarioNeto'
+    }
+];
+//Ordenar el orden de body
+body.appendChild(main);
+main.appendChild(bodyTitle);
+main.appendChild(bodySubtitle);
+
+main.appendChild(bodyInputsSection);
+bodyInputsSection.appendChild(bodyDivContainer);
+bodyDivContainer.appendChild(bodyDivSubContainer);
+bodyDivSubContainer.appendChild(bodySalarioInput);
+bodyDivSubContainer.appendChild(bodySalarioButton);
+bodyDivSubContainer.appendChild(bodySalarioUl);
+bodyDivContainer.appendChild(bodyTipoCambioInput);
+
+main.appendChild(bodyTyDSection);
+bodyTyDSection.appendChild(bodyTractosTitle);
+bodyTyDSection.appendChild(bodyTractosTable);
+bodyTractosTable.appendChild(bodyTractosTHead);
+bodyTractosTable.appendChild(bodyTractosTbody);
+
+bodyTyDSection.appendChild(bodyDetallesTitle);
+bodyTyDSection.appendChild(bodyDetallesTable);
+bodyDetallesTable.appendChild(bodyDetallesTHead);
+bodyDetallesTable.appendChild(bodyDetallesTbody);
+
+//Rellenar variables
+bodyTitle.innerHTML = "Calculadora de Deducciones";
+bodySubtitle.innerHTML = "Herramienta que realiza el calculo de deducciones salariales de Costa Rica";
+bodySalarioButton.textContent = "Dolares";
+bodyTractosTitle.innerHTML = "Tractos";
+bodyDetallesTitle.innerHTML = "Detalles";
+bodyTractosTHead.innerHTML = `
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+    `;
+bodyDetallesTHead.innerHTML = `
+                <tr>
+                    <th></th>
+                    <th></th>
+                </tr>
+    `;
+//Agregar atributos del body
+bodyDivContainer.setAttribute("class", "mb-3");
+bodyDivSubContainer.setAttribute("class", "input-group mb-3");
+
+bodySalarioInput.setAttribute("type", "number");
+bodySalarioInput.setAttribute("class", "form-control");
+bodySalarioInput.setAttribute("id", "salario");
+bodySalarioInput.setAttribute("placeholder", "Salario");
+bodySalarioInput.setAttribute("onblur", "Calcular()");
+
+bodySalarioButton.setAttribute("class", "btn btn-outline-secondary dropdown-toggle");
+bodySalarioButton.setAttribute("type", "button");
+bodySalarioButton.setAttribute("data-bs-toggle", "dropdown");
+bodySalarioButton.setAttribute("aria-expanded", "false");
+bodySalarioButton.setAttribute("id", "btnTipoCambio");
+
+bodySalarioUl.setAttribute("class", "dropdown-menu dropdown-menu-end");
+
+bodyTipoCambioInput.setAttribute("type", "number");
+bodyTipoCambioInput.setAttribute("class", "form-control");
+bodyTipoCambioInput.setAttribute("id", "tipoCambio");
+bodyTipoCambioInput.setAttribute("placeholder", "Tipo de cambio");
+
+bodyTyDSection.setAttribute("id", "table-container");
+bodyTractosTable.setAttribute("class", "table table-striped");
+
+bodyDetallesTable.setAttribute("class", "table table-striped");
+
+//Rellenar lista dropdown
+listaSalarioDropDown.forEach((lista) => {
+    bodySalarioUl.innerHTML += `
+                    <li class="nav-item">
+                        <a class="dropdown-item">${lista.value}</a> 
+                    </li>
+    `
+});
+
+//Rellenar tractos
+listaTractosTd.forEach((lista) => {
+    bodyTractosTbody.innerHTML += `
+                <tr>
+                    <td>${lista.nombre}</td>
+                    <td>${lista.porcentaje}</td>
+                    <td id="${lista.id}">${lista.monto}</td>
+                </tr>
+    `
+});
+
+//Rellenar detalles
+listaDetallesTd.forEach((lista) => {
+    bodyDetallesTbody.innerHTML += `
+                <tr>
+                    <td>${lista.nombre}</td>
+                    <td id="${lista.id}">${lista.monto}</td>
+                </tr>
+    `
+});
+
+///////////////////Fin Body//////////////////////////
